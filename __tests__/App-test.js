a5eda2bad18f2a0226ac1a377ab84b20cbb50ff4
AZelenski261 <63931640+AZelenski261@users.noreply.github.com>
2021-10-20 16:37:33 -0400

    Remove App-test.js tests
    This is a test to see if the tests in App-test.js were causing the issue on circle ci.

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,38 +4,3 @@ import App from "../App";
 
 import { fireEvent, render, waitFor } from "@testing-library/react-native";
 
-it("Renders Message 1", async () => {
-  const { getByTestId, getByText, queryByTestId, toJSON } = render(<App />);
-
-  const button = getByText("Run Test 1");
-  fireEvent.press(button);
-
-  await waitFor(() => expect(queryByTestId("printed-message1")).toBeTruthy());
-
-  expect(getByTestId("printed-message1").props.children).toBe("1");
-  expect(toJSON()).toMatchSnapshot();
-}),
-
-it("Renders Message 2", async () => {
-  const { getByTestId, getByText, queryByTestId, toJSON } = render(<App />);
-
-  const button = getByText("Run Test 2");
-  fireEvent.press(button);
-
-  await waitFor(() => expect(queryByTestId("printed-message2")).toBeTruthy());
-
-  expect(getByTestId("printed-message2").props.children).toBe("2");
-  expect(toJSON()).toMatchSnapshot();
-})
-
-it("Renders Message 3", async () => {
-  const { getByTestId, getByText, queryByTestId, toJSON } = render(<App />);
-
-  const button = getByText("Run Test 3");
-  fireEvent.press(button);
-
-  await waitFor(() => expect(queryByTestId("printed-message3")).toBeTruthy());
-
-  expect(getByTestId("printed-message3").props.children).toBe("3");
-  expect(toJSON()).toMatchSnapshot();
-});
