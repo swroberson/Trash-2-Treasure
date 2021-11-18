50334b695ef0a99fda88b557a5cf525c0ccf4553
Matthew <mattmarchio1@gmail.com>
2021-11-18 18:34:53 -0500

    added leaderboard function (work in progress)

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -63,6 +63,10 @@ const AuthProvider = ({ children }) => {
     await user.functions["addPoints"](value);
   }
 
+  const getLeaderboard = async () => {
+    await user.functions["getLeaderboard"]();
+  }
+
   // The signIn function takes an email and password and uses the
   // emailPassword authentication provider to log in.
   const signIn = async (email, password) => {
